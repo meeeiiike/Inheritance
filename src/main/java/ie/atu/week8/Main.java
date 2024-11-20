2f0a58d38671f1c2304c9bb3d02bbb7d825f728d
G00407095@atu.ie <G00407095@atu.ie>
2024-11-20 16:58:32 +0000

    created new instance of customer, "part2Customer" using the default constructor and setting all 5 values, then printing.

--- src/main/java/ie/atu/week8/Main.java
+++ src/main/java/ie/atu/week8/Main.java
@@ -2,8 +2,15 @@ package ie.atu.week8;
 
 public class Main {
     public static void main(String[] args) {
+        Customer part1Customer = new Customer("Mike", "ATU", "04493", "123ATU", true);
+        System.out.println(part1Customer);
 
-        Customer newCustomer = new Customer("Mike", "ATU", "04493", "123ATU", true);
-        System.out.println(newCustomer);
+        Customer part2Customer = new Customer();
+        part2Customer.setName("Not Mike");
+        part2Customer.setAddress("Not ATU");
+        part2Customer.setPhone("Not 04493");
+        part2Customer.setCustomerNum("Not 123ATU");
+        part2Customer.setMailingList(true);
+        System.out.println(part2Customer);
     }
 }
